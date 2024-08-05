cc4ca565e3c29c509745bd466bb4a2004423704f
Shuyang Yao <qzyaoshuyang3@pku.edu.cn>
2024-08-05 19:53:12 +0800

    fixed a critical bug

--- basic/_time_sync.pyx
+++ basic/_time_sync.pyx
@@ -18,7 +18,7 @@ cpdef cnp.ndarray[cnp.int64_t, ndim=1] _coordinate_recording_time(
         min_val = float('inf')
         min_idx = prev_index
         for j in range(prev_index, target_time.shape[0]):
-            if abs(target_time[j] - source_time[i]) < min_val:
+            if abs(target_time[j] - source_time[i]) <= min_val:
                 min_val = abs(target_time[j] - source_time[i])
                 min_idx = j
             else:
